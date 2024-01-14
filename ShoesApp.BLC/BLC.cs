6d2de4b2e71b56a6769e8e01b12180d65cfa78fe
kaoiks354 <kox354@gmail.com>
2024-01-14 23:51:52 +0100

    Added simple BLC and console app test

---
+++ ShoesApp.BLC/BLC.cs
@@ -0,0 +1,39 @@
+﻿using ShoesApp.Intefaces;
+using System.Reflection;
+
+namespace ShoesApp.BLC
+{
+    public class BLC
+    {
+        private IDAO dao;
+
+        public BLC(string libraryName)
+        {
+            Type? typeToCreate = null;
+
+            Assembly assembly = Assembly.UnsafeLoadFrom(libraryName);
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.IsAssignableTo(typeof(IDAO)))
+                {
+                    typeToCreate = type;
+                    break;
+                }
+            }
+
+            dao = (IDAO)Activator.CreateInstance(typeToCreate, null);
+        }
+
+        public IEnumerable<IFootwear> GetAllFootwear()
+        {
+            return dao.GetAllFootwear();
+        }
+
+        public IEnumerable<IProducer> GetAllProducers()
+        {
+            return dao.GetAllProducers();
+        }
+
+    }
+}
---
+++ ShoesApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using ShoesApp.Intefaces;
+
+namespace ShoesApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string libraryName = System.Configuration.ConfigurationManager.AppSettings["DAOLibraryName"];
+            BLC.BLC blc = new BLC.BLC(libraryName);
+
+            foreach (IProducer p in blc.GetAllProducers())
+            {
+                Console.WriteLine($"{p.ID}: {p.Name} ");
+            }
+            Console.WriteLine("----------------------");
+
+            foreach (IFootwear f in blc.GetAllFootwear())
+            {
+                Console.WriteLine($"{f.ID}: {f.Name} {f.Producer.Name} {f.Type}");
+            }
+
+        }
+    }
+}
